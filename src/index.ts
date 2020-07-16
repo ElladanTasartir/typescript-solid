a8b03a3c4ee17dfff644629cbe8f8dfdedef62a2
Erick Malta <erickmalta100@gmail.com>
2020-07-16 02:53:24 -0300

    docs: :pencil: Added a list of pros and cons with using SOLID

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/*
+Vantagens e desvantagens do SOLID
+
+Vantagens:
+- Código modular, cada módulo fazendo algo específico
+- Código reutilizável (D.R.Y - Don't Repeat Yourself)
+- Código testável, com baixo acoplamento, pois todas as nossas classes dependem apenas de alguém
+que consiga implementar os protolocs que dependemos, o que dá abertura para mocks
+- Baixo acoplamento e alta coesão
+- Código expansível, podemos escalar ele com facilidade pois facilmente é possível adicionar funcionalidades
+- Separation of Concerns (Separação de Conceitos)
+- Fácil manutenção, pois ao alterar algo fica mais difícil quebrar outras partes do sistema
+Desvantagens:
+- Complexidade aumenta muito
+- Quantidade de código digitado aumenta drasticamente
+- Tempo de desenvolvimento aumenta
+
+Tenha cuidado com: Y.A.G.N.I. (You're not gonna need it), KISS (Keep it simple, stupid)
+Não há problema em gerar um MVP (mínimo produto viável), simples mas funcional, mas
+com o tempo, é sempre interessante refatorar
+*/
